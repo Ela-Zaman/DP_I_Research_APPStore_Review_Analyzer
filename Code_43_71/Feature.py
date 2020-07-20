eafa660da0d982871618b75dd9e7c14acc25dd07
Ela Zaman <raziazaman@iut-dhaka.edu>
2020-07-20 20:21:33 +0600

    Classification of feature or UX v-1

--- Code_43_71/Feature.py
+++ Code_43_71/Feature.py
@@ -1,28 +1,34 @@
 import pandas as pd
 import pymysql
-from nltk.collocations import *
-from nltk.metrics.association import *
-from nltk.corpus import brown
+import numpy as np
 
 
 from nltk.corpus import wordnet
 import heapq
-import nltk
+
 
 import pandas as pd
-
-from nltk.collocations import *
-import nltk
-from nltk.corpus import stopwords
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import webtext
-from nltk.collocations import BigramCollocationFinder
-from nltk.metrics import BigramAssocMeasures
-from nltk.tokenize import sent_tokenize, word_tokenize
-
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.tree import DecisionTreeClassifier
+from nltk.classify import maxent
+from sklearn.model_selection import cross_val_score
+from nltk.classify import MaxentClassifier
 from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import accuracy_score
+from sklearn import model_selection
+
+
+import nltk
+
+from nltk.stem import WordNetLemmatizer
+
 from nltk.corpus import stopwords
 
 class get_Features:
@@ -104,7 +110,7 @@ class get_Features:
         # mean_word=[remove_stopwords(w) for w in words_list]
         # print('mean_word: ', mean_word,end='\n')
         lemmetized_word = [self.Lemmetizer(w) for w in clean if len(w)>3]
-        print("l", lemmetized_word[1])
+        # print("l", lemmetized_word[1])
 
         r=[x for x in lemmetized_word if x]
         syn_extract = [self.merge_synonyms(p) for p in r]
@@ -164,7 +170,7 @@ class get_Features:
 
         for k in word_frequency.keys():
             if word_frequency[k] > 2:
-                print(k)
+
                 f_list.append([list(k), word_frequency[k]])
         t=sorted(f_list)
         feature_lists=[]
@@ -204,21 +210,130 @@ class get_Features:
                 self.m_s[k[i]] = self.syn_list(k[i])
         return k
 
+    def get_data_features(self):
 
-    def get_features(self):
         connection = pymysql.connect(host='localhost',
-                             user='root',
-                             db='research')
+                                     user='root',
+                                     db='research')
         cursor = connection.cursor()
         sql = 'SELECT reviewId,title,comment FROM `feature_or_improvment_request_data'
         cursor.execute(sql)
         result = cursor.fetchall()
-        f=pd.DataFrame(result,columns=['r_id','title','comment'])
+        f = pd.DataFrame(result, columns=['r_id', 'title', 'comment'])
+
+        connection.close()
+        return f
+    def get_data_classification(self):
+        f = self.get_data_features()
+        f = f.assign(label='features')
+        connection = pymysql.connect(host='localhost',
+                                     user='root',
+                                     db='research')
+        cursor = connection.cursor()
+        sql = 'SELECT reviewId,title,comment FROM `not_feature_or_improvment_request_data`'
+
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        n_f = pd.DataFrame(result, columns=['r_id', 'title', 'comment'])
+        n_f = n_f.assign(label='not_features')
+        data= pd.concat([f, n_f])
+        data = data.sample(frac=1).reset_index(drop=True)
+        #print(data)
+
+
+
+
+
+        connection.close()
+        return data
+
+    def feature_vector(self,dataset):
+        wordfreq = self.unique_word_frequency(dataset)
+        most_freq = self.most_freq_words(wordfreq)
+        features = []
+        for wordslist in dataset:
+            feature_vector = {}
+            for m in most_freq:
+                # count the frequency of a word in each message
+                feature_vector[m] = wordslist.count(m)
+
+            features.append(feature_vector)
+        # create a dataframe of features
+        df = pd.DataFrame(features)
+        return df
+
+    def feature_or_User_experience_classification(self,clf):
+        lb = LabelBinarizer()
+        # Multinomial Naive Bayes
+
+        df=self.get_data_classification()
+        #print(df)
+        # feature
+        X = self.feature_vector(self.data_cleaning(df['comment']))
+        #print(X)
+        # class
+        y = df['label']
+        # Initialize the accuracy of the models
+        recall = []
+        precision = []
+        accuracy = []
+        # Task7
+        # Split the feature_vector using 10 fold cross validation
+        kf = KFold(n_splits=10, shuffle=False)
+
+        # Iterate over each train-test split
+        for train_index, test_index in kf.split(X):
+            # Split train-test
+            X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            # Task 8
+
+            # Multinomial Naive Bayes
+
+            model_n_b = clf.fit(X_train, y_train)
+            # prediction
+            test = model_n_b.predict(X_test)
+
+
+            y_train = np.array([number[0] for number in lb.fit_transform(y_train)])
+
+            accuracy = cross_val_score(clf, X_train, y_train, cv=10, scoring='accuracy')
+            recall = cross_val_score(clf, X_train, y_train, cv=10, scoring='recall')
+            precision = cross_val_score(clf, X_train, y_train, cv=10, scoring='precision')
+
+
+
+
+
+
+
+        result=[['Accuracy: ',np.mean(accuracy)],['Precision',np.mean(precision)],['Recall: ',np.mean(recall)]]
+
+        return result
+
+    def get_accuracy(self):
+        nb=MultinomialNB()
+        NB=self.feature_or_User_experience_classification(nb)
+        print('Multinomial Naive Bayes Classifier: ',NB)
+        dt = DecisionTreeClassifier()
+        DT = self.feature_or_User_experience_classification(dt)
+        print('Decision Tree Classifier: ', DT)
+
+
+
+
+
+
+    def get_features(self):
+        f=self.get_data_features()
         f['merged']=f['title'].astype(str) + f['comment']
+        f= f.assign(label='features')
+
         processed=self.data_cleaning(f['merged'])
         #un=self.unique_word_frequency(processed)
         #m_w=self.most_freq_words(un)
         #print(m_w)
+        r=self.get_data_classification()
         l=['pdf','view','pdf','watch','pdf','upload']
         colc=self.collocation(processed)
 
@@ -229,3 +344,4 @@ class get_Features:
 
 r=get_Features()
 print(r.get_features())
+r.get_accuracy()
